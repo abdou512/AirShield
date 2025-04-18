a7cf9fce2438b7e9e9d5fa3d975c179fe0a4dfa9
KCaziz <azizsellal.dz@gmail.com>
2025-04-18 17:10:44 +0100

    debut BD

--- server/routes.ts
+++ server/routes.ts
@@ -2,7 +2,7 @@ import type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage";
 import { z } from "zod";
-import { insertUserSchema, insertDataRecordSchema } from "@shared/schema";
+import {  insertDataRecordSchema } from "@shared/schema";
 
 export async function registerRoutes(app: Express): Promise<Server> {
   // Air quality proxy endpoint
--- shared/schema.ts
+++ shared/schema.ts
@@ -1,5 +1,6 @@
 import { pgTable, text, serial, integer, boolean, timestamp } from "drizzle-orm/pg-core";
-
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
@@ -14,3 +15,32 @@ export const users = pgTable("users", {
   created_at: timestamp("created_at").defaultNow().notNull(),
 });
 
+export const dataRecords = pgTable("data_records", {
+  id: serial("id").primaryKey(),
+  user_id: integer("user_id").references(() => users.id),
+  date: timestamp("date").defaultNow().notNull(),
+  aqi: integer("aqi").notNull(),
+  pollen_level: integer("pollen_level").notNull(),
+  temperature: integer("temperature").notNull(),
+  humidity: integer("humidity").notNull(),
+});
+
+export const insertUserSchema = createInsertSchema(users).pick({
+  firstname: true,
+  password: true,
+  lastname: true,
+});
+
+export const insertDataRecordSchema = createInsertSchema(dataRecords).pick({
+  user_id: true,
+  aqi: true,
+  pollen_level: true,
+  temperature: true,
+  humidity: true,
+});
+
+export type InsertUser = z.infer<typeof insertUserSchema>;
+export type User = typeof users.$inferSelect;
+
+export type InsertDataRecord = z.infer<typeof insertDataRecordSchema>;
+export type DataRecord = typeof dataRecords.$inferSelect;
